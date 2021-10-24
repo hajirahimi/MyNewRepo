b7c4c2f28c4ad37a5ece526daa5ff8d1a30bf70a
ahmad hajirahimi <ahmad.hajirahimi@gmail.com>
2021-10-24 17:03:37 -0400

    Initial commit

---
+++ Assignmet2/server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://Ahmad:BioxU3qYF7b27Ox3@mongodbserver.ybm1a.mongodb.net/businessContacts?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
